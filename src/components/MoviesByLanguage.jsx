15f620539332d6a36bb50ab2e1118864a8e9f54d
Hollgy <karlgustav.ek@hotmail.com>
2023-10-27 16:12:26 +0200

    new final

--- src/components/MoviesByLanguage.jsx
+++ src/components/MoviesByLanguage.jsx
@@ -1,6 +1,7 @@
 import { Chart as ChartJS, ArcElement, Tooltip, Legend, BarElement, LinearScale, Title, CategoryScale } from 'chart.js';
 ChartJS.register(ArcElement, BarElement, LinearScale, CategoryScale, Tooltip, Legend);
 import { Pie } from 'react-chartjs-2'
+import { motion } from 'framer-motion';
 
 import { getSpecialsConfig } from '../data/getSpecials';
 import { getFFConfig } from '../data/getFF';
@@ -22,14 +23,18 @@ const optionsFF = {
 
 const MoviesByLanguage = () => {
     return (
-        <section>
-            <h2>Specials by Language</h2>
+        <motion.section
+            initial={{ opacity: 0, }}
+            animate={{ opacity: 1, }}
+            transition={{ delay: 0.2, duration: 1, type: 'easeIn' }}>
+            <h2
+            >Specials by Language</h2>
             <Pie data={specialConfig} />
             <h2>Feature-Films by Language</h2>
             <Pie data={FFConfig} options={optionsFF} />
             <h2>Documentaries by Language</h2>
             <Pie data={documentariesConfig} />
-        </section>
+        </motion.section>
     )
 }
 
